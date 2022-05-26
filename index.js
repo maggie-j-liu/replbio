10dfad0667074e6912f44fae35ccfc641d779e2b
Maggie Liu <63619830+maggie-j-liu@users.noreply.github.com>
2022-05-26 19:18:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import ejs from "ejs"
+import fs from "fs"
+import { prefixes, suffixes } from "./utils/socialLinks.js"
+
+let settings
+try {
+    const settingsFile = fs.readFileSync("settings.json", "utf-8")
+    settings = JSON.parse(settingsFile)
+} catch (e) {
+    console.log(e)
+    throw new Error("Error reading settings.json file and parsing as JSON")
+}
+
+console.log(process.env.NODE_ENV)
+const PORT = process.env.NODE_ENV === "production" ? 3000 : 3001;
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(express.static("static"))
+
+app.get("/", (req, res) => {
+    for (const key of Object.keys(settings.content.socials)) {
+        let link = settings.content.socials[key]
+        if (link.length === 0 || (!(key in prefixes) && !(key in suffixes))) {
+            delete settings.content.socials[key]
+            continue
+        }
+        if (!link.startsWith("http")) {
+            if (key in prefixes) {
+                link = "https://" + prefixes[key] + link;
+            }
+            else if (key in suffixes) {
+                link = "https://" + link + suffixes[key];
+            }
+            settings.content.socials[key] = link;
+        }
+    }
+    res.render("index.ejs", { ...settings })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
---
+++ utils/socialLinks.js
@@ -0,0 +1,32 @@
+export const prefixes = {
+    "replit": "replit.com/@",
+    "github": "github.com/",
+    "email": "",
+    "facebook": "facebook.com/",
+    "twitter": "twitter.com/",
+    "instagram": "instagram.com/",
+    "youtube": "youtube.com/channel/",
+    "twitch": "twitch.tv/",
+    "tiktok": "tiktok.com/@",
+    "buymeacoffee": "buymeacoffee.com/",
+    "spotify": "open.spotify.com/user/",
+    "behance": "behance.net/",
+    "dribbble": "dribbble.com/",
+    "discord": "discord.com/users/",
+    "medium": "medium.com/@",
+    "reddit": "reddit.com/user/",
+    "soundcloud": "soundcloud.com/",
+    "linkedin": "linkedin.com/in/",
+    "patreon": "patreon.com/",
+    "producthunt": "producthunt.com/@",
+    "devto": "dev.to/",
+    "hashnode": "hashnode.com/@",
+    "website": "",
+    "kofi": "ko-fi.com/",
+    "devpost": "devpost.com/"
+}
+
+export const suffixes = {
+    "bandcamp": ".bandcamp.com",
+    "substack": ".substack.com",
+}
